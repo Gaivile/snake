f50ccfc1fa19fbe3d5023f9f87d986bfacac0945
Gaivile <gaivile.info3@gmail.com>
2016-10-01 18:05:16 +0100

    Fix move function

--- src/snake/core.clj
+++ src/snake/core.clj
@@ -2,45 +2,47 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+;;define world
+(def world { :width 100 :height 100})
+
 ;; define food
 (def food (atom #{}))
 
-;; spawn food at interval 0.5 (to change to 0.025 later)
-(defn spawn-food []
+(defn spawn-food
+  "Spawn food at interval 0.5 (to change to 0.025 later)"
+  []
   (when (< (rand) 0.5)
-    (swap! food conj [(rand-int 500) (rand-int 500)])))
-
-;; remove food when eaten
-(defn remove-food [meal]
-  (swap! food disj meal))
+    (swap! food conj [(rand-int (world :width)) (rand-int (world :width))])))
 
 ;; define a snake
-(def snakee (atom ()))
+(def snakee (atom []))
+
+(defn remove-food
+  "Remove food when eaten"
+  []
+  (swap! food disj (first @snakee))
+  )
 
 ;; initial position for a snake
-(swap! snakee conj '(250 250))
+(swap! snakee conj [50 50])
+
 ;; define a direction to move a snake
 (def direction (atom []))
 
 ;; set initial value for direction
 (swap! direction conj 1 0)
 
-
-;; wrap a snake around the board
-(defn wrap [i m]
+(defn wrap
+  "Wrap a snake around the board"
+  [i m]
   (loop [x i]
     (cond (< x 0) (recur (+ x m))
           (>= x m) (recur (- x m))
           :else x)))
 
-;; move a snake
-(defn move []
-  (let [new-values (for [[x y] @snakee]
-                    (map + [(wrap x 500) (wrap y 500)] @direction))]
-    (reset! snakee new-values)))
-
-;; change direction for the snake to move
-(defn turn [state event]
+(defn turn
+  "Change direction for the snake to move"
+  [state event]
   (case (:key event)
     (:w :up) (if (not= [0 1] @direction) (reset! direction [0 -1])  @direction)
     (:s :down) (if (not= [0 -1] @direction) (reset! direction [0 1])  @direction)
@@ -48,10 +50,21 @@
     (:d :right) (if (not= [-1 0] @direction) (reset! direction [1 0])  @direction)
     ))
 
+(defn move
+  "Move a snake"
+  []
+  (let [new-values (into []
+       (map + [(wrap (first (first @snakee)) (world :width)) (wrap (second (first @snakee)) (world :height))] @direction))]
+    (if (contains? @food (first @snakee))
+      (do
+        (remove-food)
+        (swap! snakee conj new-values))
+      (reset! snakee (drop-last 1 (conj @snakee new-values)))))
+      )
 
 (defn setup []
   (q/smooth)
-  (q/frame-rate 30)
+  (q/frame-rate 20)
   )
 
 (defn update [state]
@@ -60,18 +73,20 @@
   )
 
 (defn draw [state]
-  (q/background 0 0 0)
+  (let [w (/ (q/width) (world :width))
+        h (/ (q/height) (world :height))]
+    (q/background 0 0 0)
 
-  (doseq [[x y] @food]
-    (q/fill 0 255 0)
-    (q/stroke 0 255 0)
-    (q/rect x y 5 5))
+    (doseq [[x y] @food]
+      (q/fill 0 255 0)
+      (q/stroke 0 255 0)
+      (q/rect (* w x) (* h y) w h))
 
-  (doseq [[x y] @snakee]
-      (q/fill 255 0 0)
-      (q/stroke 255 0 0)
-      (q/rect x y 5 5))
-  )
+    (doseq [[x y] @snakee]
+        (q/fill 255 0 0)
+        (q/stroke 255 0 0)
+        (q/rect (* w x) (* h y) w h))
+  ))
 
 (q/defsketch snake
   :title "Snake"
